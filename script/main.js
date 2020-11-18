b6e46babdfe9a45372c8aeaf63ee1bcad6239bf2
fifisshh <fifisshh@gmail.com>
2020-11-18 13:05:48 +0800

    homepage_v3

--- script/main.js
+++ script/main.js
@@ -4,7 +4,9 @@ window.addEventListener('scroll', ()=> {
     const scrollPos = window.scrollY;
     if (scrollPos > 10) {
         header.classList.add('scrolled');
+        $("#navbar").show();
     } else {
         header.classList.remove('scrolled');
+        $("#navbar").hide();
     }
 })
