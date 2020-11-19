f7cf5a8f42f85e3a0f1a5a6f4250a906ca8708fb
fifisshh <fifisshh@gmail.com>
2020-11-19 09:55:49 +0800

    +背景圓可以動/loading

--- script/main.js
+++ script/main.js
@@ -10,3 +10,34 @@ window.addEventListener('scroll', ()=> {
         $("#navbar").hide();
     }
 })
+
+// about圓形微互動
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+  
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('.cir1-home').css('top',(220-(scrolled*.2))+'px');
+    $('.cirs1-home img').css('top',(650-(scrolled*.1))+'px');
+    $('.cir2-home').css('top',(1800-(scrolled*.5))+'px');
+    $('.cirs2-home img').css('top',(250-(scrolled*.1))+'px');
+}
+
+// Order alert
+$(".order").click(function(){
+    alert("X 此功能尚未解鎖");
+});
+
+// Loading
+$(window).on('load', function(){
+    setTimeout(removeLoader, 2500);
+    // wait for page load PLUS two seconds.
+});
+  function removeLoader(){
+      $( ".loader" ).fadeOut(500, function() {
+        // fadeOut complete. Remove the loading div
+        $( ".container" ).remove(); //makes page more lightweight 
+        document.getElementById("wrapper").style.display = "block";
+    });  
+  }
